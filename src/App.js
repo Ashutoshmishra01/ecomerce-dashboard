c2d7ad7e0118e15972ac43b1e21417c293ad8585
Ashutosh <am3558236@gmail.com>
2024-09-10 20:29:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hii</h2>
+      <Button>Button</Button>
+    </div>
+  );
+}
+
+export default App;
